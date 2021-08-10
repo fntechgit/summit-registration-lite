7ec44499eafd4d5f1fb845e6aa0e62de877f5698
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2021-08-10 11:21:59 -0300

    Add line break to purchase more tickets copy.

--- src/components/ticket-owned/index.js
+++ src/components/ticket-owned/index.js
@@ -24,7 +24,10 @@ const TicketOwnedComponent = ({ goToRegistration }) => {
                     <i className="fa fa-ticket"></i>
                 </div>
                 <span className={styles.complete}>
-                    Our records show you have a ticket(s) to this event. If you would like to purchase more, <a onClick={() => goToRegistration()}>click here</a>.
+                    Our records show you have a ticket(s) to this event.
+                </span>
+                <span className={styles.complete}>
+                    If you would like to purchase more, <a onClick={() => goToRegistration()}>click here</a>.
                 </span>
             </>
         </div>
