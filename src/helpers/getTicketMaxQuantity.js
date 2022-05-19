606b8a873050b15e5cd02b50914a345043c56e9b
Jared Hill <jaredbhill@gmail.com>
2022-05-19 11:18:24 -0500

    chore: code review mods

--- src/helpers/getTicketMaxQuantity.js
+++ src/helpers/getTicketMaxQuantity.js
@@ -1,2 +1,5 @@
+const INFINITE_QUANTITY = 999999;
+
 export const getTicketMaxQuantity = (ticket) =>
-    ticket ? Math.min(ticket.quantity_2_sell - ticket.quantity_sold, ticket.max_quantity_per_order || 999999) : 0;
+    ticket ? Math.min((ticket.quantity_2_sell || INFINITE_QUANTITY) - ticket.quantity_sold, (ticket.max_quantity_per_order || INFINITE_QUANTITY)) : 0;
+
