d464cd3cf7bba2c92fa573e074ca4ee65ad5309a
Sam Young <sam.young@fntech.com>
2023-12-12 09:16:06 -0800

    remove title, align mail button to the far left, remove gray borders

--- src/components/registration-lite.js
+++ src/components/registration-lite.js
@@ -255,7 +255,6 @@ const RegistrationLite = (
                 <div className={`${styles.outerWrapper} summit-registration-lite`}>
                     <div className={styles.innerWrapper}>
                         <div className={styles.title}>
-                            <span>{summitData.name}</span>
                             <i className="fa fa-close" aria-label="close" onClick={handleCloseClick}></i>
                         </div>
 
