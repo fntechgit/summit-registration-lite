92986a6002d8b9b0f65119d11800dfd3e533f70d
Tomás Castillo <tcastilloboireau@gmail.com>
2024-11-04 17:49:49 -0300

    add unit test, change formatting function
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/ticket-owned/index.js
+++ src/components/ticket-owned/index.js
@@ -14,23 +14,35 @@
 import React, { useMemo } from 'react';
 import styles from "./index.module.scss";
 
-const TicketOwnedComponent = ({ ownedTickets}) => {
+const TicketOwnedComponent = ({ ownedTickets }) => {
 
-    const ownedTicketsString = useMemo(() => ownedTickets.reduce((acc, ownedTicket, index) => {
-        const lastTicket = index + 1 === ownedTickets.length;
-        const separator = acc && lastTicket ? ' and ' : ', ';
-        const qty = ownedTicket.qty;
-        const typeName = ownedTicket.type_name;
-        // Adds ticket to the end of type name if is not included
-        const typeNameSuffix = !typeName.toLowerCase().includes('ticket') ? ' Ticket' : '';        
-        const pluralSuffix = typeNameSuffix && qty > 1 ? 's' : '';
-        
-        return `${acc}${acc ? separator : ''}${qty} ${typeName}${typeNameSuffix}${pluralSuffix}`;
-    }, ''), [ownedTickets]);
+    const formatOwnedTicketStrings = (tickets) => (
+        tickets.reduce((acc, ownedTicket, index) => {
+            const { qty, type_name } = ownedTicket;
+            const firstTicket = index === 0;
+            const lastTicket = index + 1 === tickets.length;
+            const separator = acc ? (lastTicket ? ' and ' : ', ') : '';
+
+            let updatedTypeName = type_name;
+
+            if (firstTicket) {
+                // For the first ticket, replace 'ticket' with 'Ticket(s)' or add ' Ticket(s)'
+                if (/ticket/i.test(type_name)) {
+                    updatedTypeName = type_name.replace(/ticket/i, 'Ticket(s)');
+                } else {
+                    updatedTypeName += ' Ticket(s)';
+                }
+            }
+
+            return `${acc}${separator}${qty} ${updatedTypeName}`;
+        }, '')
+    );
+
+    const ownedTicketsString = useMemo(() => formatOwnedTicketStrings(ownedTickets), [ownedTickets]);
 
     return (
         <div className={styles.ticketOwnedWrapper}>
-            <div className={`${styles.alert} alert alert-warning`} role="alert">
+            <div className={`${styles.alert} alert alert-warning`} role="alert" data-testid="owned-tickets">
                 You have already ordered {ownedTicketsString}. If you would like to order more, please do so below.
             </div>
         </div>
