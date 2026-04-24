6d7bb2b34f9794fa4bfd13c866a41844462ee8f5
smarcet <smarcet@gmail.com>
2026-04-24 17:31:51 -0300

    chore(unit-test): fix unit test

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,12 @@
 import 'regenerator-runtime/runtime'
+import { TextEncoder, TextDecoder } from 'util';
+import T from 'i18n-react';
+
+if (typeof globalThis.TextEncoder === 'undefined') {
+    globalThis.TextEncoder = TextEncoder;
+    globalThis.TextDecoder = TextDecoder;
+}
+
+T.setTexts(require('./src/i18n/en.json'));
 
 jest.setTimeout(10000); // in milliseconds
